44764c8aed76b7bc624030840f15668652a62ed1
Agusef22 <Agusef22@gmail.com>
2022-10-17 03:26:16 -0300

    solucione problemas

--- script.js
+++ script.js
@@ -39,94 +39,10 @@ iconProductClose.addEventListener("click", () => {
     productDetail.classList.toggle("inactive");
 });
 
-
-const productList = [];
 const orderList = [];
 
-productList.push({
-    id: 1,
-    name: "Macbook Air",
-    price: 1000,
-    image: "./img/macbook.jpeg",
-    description: "Macbook Air viene con el intel core i5, con 250gb ssd"
-});
-orderList.push({
-    name: "Macbook Air",
-    price: 1000,
-    image: "./img/macbook.jpeg"
-});
 
-productList.push({
-    id: 2,
-    name: "Notebook Msi",
-    price: 1100,
-    image: "./img/compu.jpg",
-    description: "Notebook Msi con 500gb ssd, Geforce 2060 super."
-});
-
-orderList.push({
-    name: "Notebook Msi",
-    price: 1100,
-    image: "./img/compu.jpg"
-});
-
-productList.push({
-    id: 3,
-    name: "Parlante JBL",
-    price: 200,
-    image: "./img/JBL.webp",
-    description: "Parlante JBL con muy poco uso, en excelente estado, se escucha perfectamente bien."
-});
-orderList.push({
-    name: "Parlante JBL",
-    price: 200,
-    image: "./img/JBL.webp"
-});
     
-productList.push({
-    id: 4,
-    name: "Apple Watch Pro",
-    price: 500,
-    image: "./img/watch.jpg",
-    description: "Apple Watch Pro, viene genial para hacer deporte, es del ano 2019"
-});
-orderList.push({
-    name: "Apple Watch Pro",
-    price: 500,
-    image: "./img/watch.jpg"
-});
-
-productList.push({
-    id: 5,
-    name: "Mochila Mediana",
-    price: 100,
-    image: "./img/mochila.jpg",
-    description: "Mochila mediana, ideal para el uso escolar"
-});
-
-productList.push({
-    id: 6,
-    name: "Joystick PS5",
-    price: 150,
-    image: "./img/joystick.webp",
-    description: "Joystick PS5 en perfecto estado, con muy pocos meses de uso"
-});
-
-productList.push({
-    id: 7,
-    name: "Auriculares Samsung",
-    price: 300,
-    image: "./img/auris.jpg",
-    description: "Auriculares Samsung del ano 2019, en excelente estado, muy poco uso"
-});
-
-productList.push({
-    id: 8,
-    name: "Camara Profesional",
-    price: 1500,
-    image: "./img/camara.jpeg",
-    description: "Camara Profesional ideal para gente que quiera hacer fotos con una alta definicion"
-});
 
 /* 
 <div class="container-productos">
@@ -139,13 +55,8 @@ productList.push({
 </div> 
 */
 
-const renderProducts = (arr) => {
-    for (product of arr) {
-        imageProductDetail.src = product.image;
-        priceProduct.innerText = "$" + product.price;
-        nameProduct.innerText = product.name;
-        descriptionProduct.innerText = product.description;
-
+productList.forEach((product) => {
+    
         const infoProduct = document.createElement("div");
         infoProduct.classList.add("container-info-product");
 
@@ -154,6 +65,10 @@ const renderProducts = (arr) => {
         productImg.setAttribute("src", product.image);
         productImg.classList.add("mac-image");
         productImg.addEventListener("click", () => {
+            imageProductDetail.src = product.image;
+            priceProduct.innerText = "$" + product.price;
+            nameProduct.innerText = product.name;
+            descriptionProduct.innerText = product.description;
 
             darken.classList.toggle("inactive");
             productDetail.classList.toggle("inactive");
@@ -170,7 +85,16 @@ const renderProducts = (arr) => {
         const button = document.createElement("button");
         button.innerText = "Agregar";
         button.id = product.id
-        
+
+        button.addEventListener("click", () => {
+            const agregarCarrito = (prodId) => {
+                const item = productList.find((prod) => prod.id === prodId)
+                orderList.push(item)
+                uploadCart()
+                console.log(orderList)
+            }
+            agregarCarrito(product.id)
+        })
         
 
         infoProduct.appendChild(productImg);
@@ -179,8 +103,7 @@ const renderProducts = (arr) => {
         infoProduct.appendChild(button);
         products.appendChild(infoProduct)
         
-    }
-};
+});
 
 /* 
 <figure>
@@ -191,10 +114,10 @@ const renderProducts = (arr) => {
 <img src="./icon/icon_close.png" alt="close"></img>
 */
 
+const uploadCart = () => {
+    orderContent.innerHTML = "";
 
-
-const renderProductsOrder = (arr) => {
-    for (product of arr) {
+    orderList.forEach((product) => {
         const shoppingCart = document.createElement("div");
         shoppingCart.classList.add("shopping-cart");
 
@@ -219,91 +142,14 @@ const renderProductsOrder = (arr) => {
         shoppingCart.appendChild(priceProduct);
         shoppingCart.appendChild(icon);
         orderContent.appendChild(shoppingCart);
+        renderOrderPrice(orderList)
 
-        
-    }
 
-};
+});
+}
 
 const renderOrderPrice = (arr) => {
     const sum = arr.map(item => item.price).reduce((prev, curr) => prev + curr, 0);
     totalPrice.innerText = "$" + sum;
 };
 
-/* 
-<div class="product-detail-close">
-    <img src="./icon/icon_close.png" alt="close">
-</div>
-<img src="./img/JBL.webp" alt="bike">
-<div class="product-info">
-    <p>$35,00</p>
-    <p> Parlante JBL</p>
-    <p>Parlante JBL con muy poco uso, en excelente estado, se escucha perfectamente bien.</p>
-    <button class="primary-button add-to-cart-button">
-    <img class="add-cart" src="./icon/bt_add_to_cart.svg" alt="add to cart">
-    Add to cart
-    </button>
-</div> 
-*/
-
-// const creatProductDetail = (arr) => {
-//     for (product of arr) {
-//         // const productDetailAside = document.createElement("aside");
-//         // productDetailAside.id = "productDetail";
-//         // productDetailAside.classList.add("inactive");
-
-//         const productClose = document.createElement("div");
-//         productClose.classList.add("product-detail-close");
-
-//         const iconProductClose = document.createElement("img");
-//         iconProductClose.src = "./icon/icon_close.png";
-
-//         productClose.appendChild(iconProductClose);
-
-//         const imgProductDetail = document.createElement("img");
-//         imgProductDetail.src = product.image;
-//         // imgProductDetail.addEventListener("click", () => {
-
-//         //     darken.classList.toggle("inactive");
-//         //     productDetail.classList.toggle("inactive");
-//         // });
-
-//         const productDetailInfo = document.createElement("div");
-//         productDetailInfo.classList.add("product-info");
-
-//         const priceProduct = document.createElement("p");
-//         priceProduct.innerText = "$" + product.price;
-
-//         const nameProduct = document.createElement("p");
-//         nameProduct.innerText = product.name;
-
-//         const descriptionProduct = document.createElement("p");
-//         descriptionProduct.innerText = product.description;
-
-//         const buttonProduct = document.createElement("button");
-//         buttonProduct.classList.add("primary-button");
-//         buttonProduct.innerText = "Add to cart";
-
-//         const imgCartProduct = document.createElement("img");
-//         imgCartProduct.classList.add("add-cart");
-//         imgCartProduct.src = "./icon/bt_add_to_cart.svg";
-
-//         buttonProduct.appendChild(imgCartProduct);
-//         productDetailInfo.appendChild(priceProduct);
-//         productDetailInfo.appendChild(nameProduct);
-//         productDetailInfo.appendChild(descriptionProduct);
-//         productDetailInfo.appendChild(buttonProduct);
-//         productDetail.appendChild(productClose);
-//         productDetail.appendChild(imgProductDetail);
-//         productDetail.appendChild(productDetailInfo);
-
-
-//     }
-    
-// };
-
-
-renderProducts(productList);
-renderProductsOrder(orderList);
-renderOrderPrice(orderList);
-// creatProductDetail(productList);
---
+++ stock.js
@@ -0,0 +1,10 @@
+let productList = [
+    {id: 1, name: "Macbook Air", price: 1000, image: "./img/macbook.jpeg", description: "Macbook Air viene con el intel core i5, con 250gb ssd"},
+    {id: 2, name: "Notebook Msi", price: 1100, image: "./img/compu.jpg", description: "Notebook Msi con 500gb ssd, Geforce 2060 super."},
+    {id: 3, name: "Parlante JBL", price: 200, image: "./img/JBL.webp", description: "Parlante JBL con muy poco uso, en excelente estado, se escucha perfectamente bien."},
+    {id: 4, name: "Apple Watch Pro", price: 500, image: "./img/watch.jpg", description: "Apple Watch Pro, viene genial para hacer deporte, es del ano 2019"},
+    {id: 5, name: "Mochila Mediana", price: 100, image: "./img/mochila.jpg", description: "Mochila mediana, ideal para el uso escolar"},
+    {id: 6, name: "Joystick PS5", price: 150, image: "./img/joystick.webp", description: "Joystick PS5 en perfecto estado, con muy pocos meses de uso"},
+    {id: 7, name: "Auriculares Samsung", price: 300, image: "./img/auris.jpg", description: "Auriculares Samsung del ano 2019, en excelente estado, muy poco uso"},
+    {id: 8, name: "Camara Profesional", price: 1500, image: "./img/camara.jpeg", description: "Camara Profesional ideal para gente que quiera hacer fotos con una alta definicion"}
+]
